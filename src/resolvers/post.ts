8d3467bc48c6b8913b37ce6e0054d8c9a32aac84
huangsri <Peerawas.Likit@gmail.com>
2021-06-24 19:35:53 +0700

    6:31:26 URQL Pagination Start

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -9,6 +9,7 @@ import {
   Resolver,
   UseMiddleware,
 } from 'type-graphql'
+import { getConnection } from 'typeorm'
 
 import { Post } from '../entities/Post'
 import { isAuth } from '../middleware'
@@ -26,8 +27,23 @@ class PostInput {
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(): Promise<Post[]> {
-    return Post.find()
+  posts(
+    @Arg('limit', () => Int) limit: number,
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(50, limit)
+
+    const qb = getConnection()
+      .getRepository(Post)
+      .createQueryBuilder('p')
+      .orderBy('"createdAt"', 'DESC')
+      .take(realLimit)
+
+    if (cursor) {
+      qb.where('"createdAt" < :cursor', { cursor: new Date(Number(cursor)) })
+    }
+
+    return qb.getMany()
   }
 
   @Query(() => Post, { nullable: true }) // for graph type
